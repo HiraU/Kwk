77502125d277bb7ab3949b1488cc5d697ea3da7e
HiraU <109610922+HiraU@users.noreply.github.com>
2023-01-22 20:13:20 -0500

    Create Hira.swift

---
+++ Hira.swift
@@ -0,0 +1,7 @@
+ func calculator (firstNum: Int, secondNum: Int){
+   let adding = firstNum + secondNum
+   let difference = firstNum - secondnum
+let product = firstNum x secondNum
+  let quotient = firstNum / secondNum 
+   let results = "The sum of \(firstNum) and (secondNum) is \(sum), "
+ }
